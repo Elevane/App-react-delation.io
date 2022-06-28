66c30c3ea6dffad0e6bfc67909509e5b539caeeb
bastien aubry <pma.bastien.aubry@gmail.com>
2022-06-28 22:44:06 +0200

    feat() : - on ne peut pas appuyer plei nde fois sur le bouton célarer et ainsi créer plusieurs fois la meme délation - on ne peut export que si des delations sont disponibles - bouton resfresh

--- src/App.js
+++ src/App.js
@@ -9,34 +9,51 @@ function App() {
   const [name, setName] = useState("");
   const [action, setAction] = useState("");
   const [pdfData, setPdfData] = useState(null);
+  const [activeButton, setActiveButton] = useState("disabled");
+  const [acttiveExport, setActiveExport] = useState("disabled");
+  useEffect(() => {
+    if(action != null && name != null && action != undefined && name != undefined && action != "" && name  != "")
+    {
+      setActiveButton(" ")
+    }
+  }, [name, action])
 
+useEffect(() => {
+  setPdfData(document.getElementById("delationbs"))
+}, [delationList])
 
   useEffect(()=> {
       fetch(process.env.REACT_APP_API_URL)
       .then((response) => response.json())
       .then(
         (delations) => {
-          if(Object.keys(delations.delations).length > 0)
+          if(Object.keys(delations.delations).length > 0){
             setDelationList(delations.delations)
+          }
+          
+        if(Object.keys(delations.delations).length == 0){
+          setActiveExport("disabled")
+          
         }
-      )
+        else{
+          setActiveExport(" ")
+        }
+      }
+      )    
   }, []);
 
-  useEffect(() => {
-    setPdfData(document.getElementById('delationbs'));
-  }, [delationList])
+
 
   const exportPDFWithMethod = () => {
-    if(delationList.length > 0){
+    
+    if(delationList.length > 0 && acttiveExport === " "){
       savePDF(pdfData, {
         paperSize: "auto",
         margin: 40,
         fileName: `Report for ${new Date().getDate()}`,
       });
     }
-    else{
-      alert("aucune delation a reporter")
-    }
+    
   };
 
   let itemList= delationList.map((item,index)=>{
@@ -46,6 +63,9 @@ function App() {
               <p className='date'>{new Date(item.date_delation).getUTCDate() +"/"+ new Date(item.date_delation).getUTCMonth()+"/"+new Date(item.date_delation).getUTCFullYear()}</p><p>.</p>
   </li>
   })
+
+
+
   const getIdHash = () => {
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -59,35 +79,60 @@ function App() {
 
 
   const sendDelation = (del) => {
-      fetch(process.env.REACT_APP_API_URL,{
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        Accept: "*/*",
-      },
-      body: JSON.stringify(del),
-    })
-    .then((data) => data.json()).then(()=> {
-      const newList = [...delationList, del];
-      setDelationList(newList);
-      clearFields();
-    });
+    
+        fetch(process.env.REACT_APP_API_URL,{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          Accept: "*/*",
+        },
+        body: JSON.stringify(del),
+      })
+      .then((data) => data.json()).then(()=> {
+        
+      });
+    
   }
   
   const declare = () => {
-    if(action === null || name === null || action === undefined || name === undefined)
-      alert("un des chamsp est null")
-    let delation = {
-      "id" : getIdHash(),
-      "name" : name,
-      "actionname" : action,
-      "date_delation" : new Date()
-    };
-    sendDelation(delation)
-    
+    if( activeButton === " "){
+      let toCreate = {
+        "id" : getIdHash(),
+        "name" : name,
+        "actionname" : action,
+        "date_delation" : new Date()
+      };
+      setActiveButton("disabled")
+      sendDelation(toCreate)
+      const newList = [...delationList, toCreate];
+      setDelationList(newList);
+      clearFields();
+    }  
   };
-
+  const refresh = () => {
+    setActiveButton("disabled")
+    setActiveExport("disabled")
+    fetch(process.env.REACT_APP_API_URL)
+    .then((response) => response.json())
+    .then(
+      (delations) => {
+        if(Object.keys(delations.delations).length > 0){
+          setDelationList(delations.delations)
+          console.log(delations.delations)
+        }
+        
+      if(Object.keys(delations.delations).length == 0){
+        setActiveExport("disabled")
+        
+      }
+      else{
+        setActiveExport(" ")
+      }
+    }
+    )    
+    
+  }
   const clearFields = () => {
     setAction("");
     setName("");
@@ -103,11 +148,12 @@ function App() {
                 <div className="col-md-12">
                     <div className="card px-3">
                         <div className="card-body">
-                            <div className="add-items d-flex"> 
+                            <div className="add-items d-flex">
                               <input type='text' onChange={(e) => setName(e.target.value)}className="form-control todo-list-input name" value={name} placeholder="Qui c'est encore ?"/> 
                               <input type="text" value={action} onChange={(e) => setAction(e.target.value)} className="form-control todo-list-input" placeholder="Qu'a t-il.elle encore fait ?"/> 
-                              <button onClick={() => declare()}className="add btn btn-primary font-weight-bold todo-list-add-btn">Déclarer</button> 
-                              <button onClick={() => exportPDFWithMethod()} className="add btn btn-warning font-weight-bold todo-list-add-btn">Export</button> 
+                              <button onClick={() => {if(activeButton !== "disabled") declare()}}className={"add btn btn-primary font-weight-bold todo-list-add-btn "+ activeButton}>Déclarer</button> 
+                              <button onClick={() => exportPDFWithMethod()} className={"add btn btn-warning font-weight-bold todo-list-add-btn "+ acttiveExport} >Export</button> 
+                              <button onClick={() => refresh()} className={"add btn btn-primary font-weight-bold todo-list-add-btn "} ><i className='bi bi-arrow-clockwise refresh'></i></button> 
                             </div>
                             <div className="list-wrapper">
                                 <ul id="delationbs" className=" delationbs d-flex flex-column-reverse todo-list">
